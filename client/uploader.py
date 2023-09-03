c4948950e1637809dbbb3bb679d878718c5ae83b
neon145 <aj.sendtext@gmail.com>
2023-09-03 19:32:53 +0530

    Petros is a success

--- client/uploader.py
+++ client/uploader.py
@@ -1,14 +1,23 @@
-import os
-directory = "test"
-struct =  []
-def recSearch(directory) :
-    for file in os.listdir(directory):
-        f = os.path.join(directory, file)
-        print(f)
-        if (os.path.isfile(f)):
-            struct.append(f)
-        if (os.path.isdir(f)):
-            recSearch(f)
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isalpha():  # Check if the character is a letter
+            is_upper = char.isupper()  # Check if the letter is uppercase
+            char = char.lower()  # Convert to lowercase for simplicity
+            shifted_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
+            
+            if is_upper:
+                shifted_char = shifted_char.upper()  # Convert back to uppercase if it was uppercase in the original text
+            
+            encrypted_text += shifted_char
+        else:
+            encrypted_text += char  # Keep non-alphabet characters unchanged
+    
+    return encrypted_text
 
-recSearch("test")
-print(struct)
+def caesar_decipher(encrypted_text, shift):
+    return caesar_cipher(encrypted_text, -shift)  # Deciphering is just shifting in the opposite direction
+
+
+print(caesar_decipher("Neon145",2))
